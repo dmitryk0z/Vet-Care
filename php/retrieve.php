ddf12a24e9e1ebd2f6a199a2e138f788f91dcd84
dmitryk0z <79474744+dmitryk0z@users.noreply.github.com>
2022-08-24 12:50:30 +0100

    Add files via upload

---
+++ php/retrieve.php
@@ -0,0 +1,91 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../css/style.css">
+    <title>FIND PET</title>
+</head>
+<body>
+    <h3>MENU</h3>
+
+    <br><br>
+    <a href="insert.php"><button>ADD NEW PET</button></a>
+    <a href="update.php"><button>UPDATE PET</button></a>
+    <a href="../index.php"><button>ALL PETS</button></a>
+    <a href="retrieve.php"><button id="update-btn">FIND PET</button></a>
+    <a href="delete.php"><button>DELETE PET</button></a>
+    <br><br>
+    
+    <form action="../php/retrieve.php" method="get">
+        <h4>FIND PET IN THE DATABASE</h4>
+        <div class="container2">
+            <label for="findPet">Search Pet by ID, Name, Species, Weight, Ailment, Owner or Vet:</label>
+            <input type="text" name="findPet" id="findPet" required>
+            <input name='submit' type='submit' value='SUBMIT' id='input-btn'>
+        </div>
+    </form>
+
+    <?php
+        if (isset($_GET['submit'])) {
+            $conn = mysqli_connect("localhost", "root", "", "vet_clinic_db");
+                
+            if ($conn === false) {
+                die("ERROR: Could not connect..."
+                .mysqli_connect_error());
+            }
+
+            $findPet = $_REQUEST['findPet'];
+
+            try {
+                $sql = "SELECT PetID, PetName, PetSpecies, PetWeight,
+                PetAilment, pet_owner.OwnerFirstName, pet_owner.OwnerLastName,
+                vet.VetFirstName, vet.VetLastName
+                FROM pet
+                JOIN vet
+                ON pet.VetID = vet.VetID
+                JOIN pet_owner
+                ON pet.OwnerID = pet_owner.OwnerID
+                WHERE PetID = '$findPet' OR PetName = '$findPet' OR PetSpecies = '$findPet' OR 
+                PetWeight = '$findPet' OR PetAilment = '$findPet' OR pet_owner.OwnerFirstName = '$findPet' 
+                OR pet_owner.OwnerLastName = '$findPet' OR vet.VetFirstName = '$findPet' 
+                OR vet.VetLastName = '$findPet'";
+                $result = mysqli_query($conn, $sql);
+
+                if(mysqli_num_rows($result) != 0) {
+                    echo
+                    "<table id='records_table'>
+                    <tr>
+                    <th>Pet ID</th>
+                    <th>Pet Name</th>
+                    <th>Pet Species</th>
+                    <th>Pet Weight</th>
+                    <th>Pet Ailment</th>
+                    <th>Owner Name</th>
+                    <th>Vet Name</th>
+                    </tr>";
+                    while($row = mysqli_fetch_array($result)) {
+                        echo "<tr>";
+                        echo "<td>" . $row['PetID'] . "</td>";
+                        echo "<td>" . $row['PetName'] . "</td>";
+                        echo "<td>" . $row['PetSpecies'] . "</td>";
+                        echo "<td>" . $row['PetWeight'] . "</td>";
+                        echo "<td>" . $row['PetAilment'] . "</td>";
+                        echo "<td>" . $row['OwnerFirstName'] . " " . $row['OwnerLastName'] . "</td>";
+                        echo "<td>" . $row['VetFirstName'] . " " . $row['VetLastName'] . "</td>";
+                        echo "</tr>";
+                    }
+                    echo "</table>";
+                } else {
+                    echo "<h3>No pet records found in the Database!</h3>";
+                }
+            } catch (Exception) {
+                echo "<h3>Something went wrong - Ensure that all tables were created correctly</h3>";
+            }
+            
+            mysqli_close($conn);
+        }
+    ?>
+</body>
+</html>
